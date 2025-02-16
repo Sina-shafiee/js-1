78fe665f964394cbf59f6ef13cd14ab5ce595831
Sina Shafiee <alisinashafieezada@gmail.com>
2025-02-16 21:15:09 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// variables
+// KEYWORD
+// let numberOne = 2;
+
+// let numberTwo = 4;
+
+// let userName = 'Zana';
+
+// const numberThree = 3;
+
+// data type
+// console.log(numberOne * numberTwo);
+
+// Not a number
+
+// CONST VS LET
+// const numberOne = 3424234;
+
+// let numberTwo = 234234;
+// numberTwo = 'Zana';
+
+// console.log(numberOne);
+
+// TYPEOF
+// const numberOne = 3424234;
+// console.log(typeof numberOne);
+
+// BOOLEAN
+// const isBig = true;
+// const isGirl = false;
+
+// console.log(isGirl);
+// false
+// user name
+// const user_name = 'sdasd'; // snake case
+// const userNameFirst = 'sdasd'; // camel case
+// const firstName = 'zana';
+// const isLoggedIn = true;
+// const class = "sadsd"
+
+// const numberOne = 2;
+
+// console.log(typeof numberOne);
+
+// STRINGS
+
+// const userName = 'hi how are you';
+
+// console.log(userName);
+// console.log(userName.length);
+// console.log(userName[0]);
+
+// const message = 'hi how are you';
+// const userName = 'Zana';
+
+// const welcomeMessage = message + ' ' + userName;
+
+// console.log(message + ' ' + userName);
+// console.log(welcomeMessage);
+
+// const numberOne = 2;
+
+// const somthing = message + numberOne;
+
+// console.log(typeof somthing);
+
+// STRING METHODS
+// const message = '  hi how are you   ';
+// const email = '   sina@gmail.com';
+// const userName = 'Zana';
+
+// const trimedMessage = message.trim().toUpperCase();
+
+// console.log(trimedMessage);
+// console.log(userName.toLowerCase());
+// console.log(email.trim());
